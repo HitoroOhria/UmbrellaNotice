67949dfdbb9a954ae45de3286836d44e0219f4ce
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-28 14:26:20 +0900

    rails g migration AddOmniauthToUsers provider:string uid:string image:string name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_083213) do
+ActiveRecord::Schema.define(version: 2020_04_28_052250) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2020_04_27_083213) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "image"
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
