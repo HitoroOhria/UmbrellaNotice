cb48354eda82c8fa84b417c1e175a12973092580
大平 礼 <hiroto100114@gmail.com>
2020-05-13 20:30:35 +0900

    [fix]CI環境にopenjdk-8-jdkをinstallする設定を記述
      rspec:
        - Dockerfile.ci
          - ENV JAVA_HOME /usr/lib/jvm/java-1.8.0-openjdk-amd64
          - apt-get install -y openjdk-8-jdk
      rubocop:
        - run:
            - sudo apt-get install -y openjdk-8-jdk

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -18,7 +18,7 @@ class WeathersController < ApplicationController
   # 引数の天気予報の内容から、雨が降るか判定
   def today_is_rainy?(json_weathers)
     rainy_weathers = json_weathers[:list].select do |weather|
-      weather[:rain].present? ? weather[:rain][:'3h'] : false
+      weather[:rain].present? ? weather[:rain][:'3h'] : nil
     end
     rainy_weathers.find { |weather| weather[:rain][:'3h'] >= 2 } .present?
   end
