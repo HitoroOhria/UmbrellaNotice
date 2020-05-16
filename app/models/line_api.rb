83e10800b5fe352cd1a05b65ba78ee81e105029b
大平 礼 <hiroto100114@gmail.com>
2020-05-16 12:31:54 +0900

    [add]LineApiモデルにself.find_or_create_line(line_id)を追加

--- app/models/line_api.rb
+++ app/models/line_api.rb
@@ -1,2 +1,9 @@
 class LineApi < ApplicationRecord
+  belongs_to :user
+
+  validates :notice_time, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2400 }
+
+  def self.find_or_create_line(line_id)
+    LineApi.find_by(line_id: line_id) || LineApi.create(line_id: line_id)
+  end
 end
