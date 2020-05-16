7d12ebb2ad660813e7f5bf253fe7afe38e99cbab
大平 礼 <hiroto100114@gmail.com>
2020-05-16 12:11:24 +0900

    [update]WeatherApiに3つのメソッドを追加
      - city_varidation
      - to_romaji(text)
      - forecast

--- app/controllers/weather_api_controller.rb
+++ app/controllers/weather_api_controller.rb
@@ -1,19 +1,11 @@
-class WeatherAPIController < ApplicationController
+class WeatherApiController < ApplicationController
   def notice
-    @weather_forecast = json_weather_infomation('おしゃまんべ')
+    user = LineApi.find_by(line_id: params[:hash_line_id])
+    @weather_forecast = user.weather_api.forecast
     @rain_notice = today_is_rainy?(@weather_forecast)
   end
 
   private
-
-  # OpenWeatherAPI から、引数の市名の天気予報を取得
-  def json_weather_infomation(city_name)
-    romaji_city_name = Zipang.to_slug(city_name).gsub(/\-/, '').gsub(/m(?!(a|i|u|e|o|m))/, 'n').to_kunrei
-    base_url = 'http://api.openweathermap.org/data/2.5/forecast'
-    response = OpenURI.open_uri(base_url + "?cnt=8&q=#{romaji_city_name},jp" \
-                                 + "&appid=#{Rails.application.credentials.open_weather_api[:app_key]}")
-    JSON.parse(response.read, symbolize_names: true)
-  end
 
   # 引数の天気予報の内容から、雨が降るか判定
   def today_is_rainy?(json_weathers)
--- app/models/weather_api.rb
+++ app/models/weather_api.rb
@@ -1,2 +1,35 @@
 class WeatherApi < ApplicationRecord
+  belongs_to :user
+
+  validates :lat, numericality: { greater_than_or_equal_to: -45, less_than_or_equal_to: 45 }
+  validates :lon, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
+  def city_varidation
+    self.city = to_romaji(city)
+    forecast ? self : WeatherApi.new
+  end
+
+  def to_romaji(text)
+    Zipang.to_slug(text).gsub(/\-/, '').gsub(/m(?!(a|i|u|e|o|m))/, 'n').to_kunrei
+  end
+
+  # OpenWeatherAPI から、天気予報を取得
+  def forecast
+    base_url = 'http://api.openweathermap.org/data/2.5/forecast'
+
+    if (request_query = city.present?)
+      request_query = "?cnt=8&q=#{city},jp"
+    else
+      request_query = "?cnt=8&lat=#{lat}&lon=#{lon}"
+    end
+
+    begin
+      response = OpenURI.open_uri(base_url + request_query \
+                                   + "&appid=#{Rails.application.credentials.open_weather_api[:app_key]}")
+      JSON.parse(response.read, symbolize_names: true)
+    end
+    rescue OpenURI::HTTPError
+      # invalid_city
+    end
+  end
 end
