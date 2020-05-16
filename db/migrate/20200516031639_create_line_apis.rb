6a35385da0fd5f04e64d8e075417463850a785f0
大平 礼 <hiroto100114@gmail.com>
2020-05-16 12:30:12 +0900

    [add]LineApiのlocatableカラムのデフォルトをfalseに設定

--- db/migrate/20200516031639_create_line_apis.rb
+++ db/migrate/20200516031639_create_line_apis.rb
@@ -1,7 +1,7 @@
 class CreateLineApis < ActiveRecord::Migration[5.2]
   def change
     create_table :line_apis do |t|
-      t.boolean :locatable
+      t.boolean :locatable, default: false, null: false
       t.integer :notice_time
       t.string :line_id
 
