9db04526b6414966027568eee1fdfaf692da0ed4
大平 礼 <hiroto100114@gmail.com>
2020-05-29 11:37:14 +0900

    [fix]WeatherApiモデルの改名に従い、seedsファイルの内容を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 user = User.create!(email: 'hogehoge@hoge.com', password: 'foobar', confirmed_at: Time.zone.now)
 line_user = LineUser.create!(line_id: '123hoge')
-WeatherApi.create!(user: user, line_user: line_user, city: 'osyamanbe', lat:42.53 , lon: 140.31)
+Weather.create!(user: user, line_user: line_user, city: 'shibuya', lat:42.53 , lon: 140.31)
