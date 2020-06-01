78cb7d752eccca7578b33cca9a457ea9aecd4ba5
大平 礼 <hiroto100114@gmail.com>
2020-06-01 20:51:53 +0900

    [add]production用のsidekiqのイニチアライザを追加

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
 Sidekiq.configure_server do |config|
-  config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq'}
+  case Rails.env
+  when 'production'
+    config.redis = {url: 'redis://www.umbrellanotice.work:6379', namespace: 'sidekiq'}
+  else
+    config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq'}
+  end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq'}
+  case Rails.env
+  when 'production'
+    config.redis = {url: 'redis://www.umbrellanotice.work:6379', namespace: 'sidekiq'}
+  else
+    config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq'}
+  end
 end
