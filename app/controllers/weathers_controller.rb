5baa528c4022d47475f681407ab179d956810998
大平 礼 <hiroto100114@gmail.com>
2020-06-01 10:54:41 +0900

    [add]傘通知を行う処理をWeathersControllerに実装
      - #trigger
      - #notice
      bofore_action:
        - #validate_notice_time
        - #authenticate

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,21 +1,57 @@
 class WeathersController < ApplicationController
-  def trigger
+  before_action :validate_notice_time, only: [:trigger]
+  before_action :authenticate,         only: [:notice]
 
+  RAIN_FALL_JUDGMENT = 2
+  TOLERANCE_TIME = 3
+
+  def trigger
+    line_users = LineUser.where(notice_time: params[:notice_time])
+    line_users.each do |line_user|
+      post weathers_notice_url, headers: { 'Authorization': "Token #{line_user.token}" },
+                                params:  { line_id: line_user.line_id }
+    end
+    render_success
   end
 
   def notice
-    line_user = LineUser.first
-    @weather_forecast = line_user.weather_api.take_forecast
-    @rain_notice = today_is_rainy?(@weather_forecast)
+    line_user = LineUser.find_by(line_id: params[:line_id])
+    weather_forecast = line_user.weather.take_forecast
+    umbrella_notice(line_user.line_id) if today_is_rainy?(weather_forecast)
+    render_success
   end
 
   private
 
-  # 引数の天気予報の内容から、雨が降るか判定
   def today_is_rainy?(json_weathers)
-    rainy_weathers = json_weathers[:list].select do |weather|
+    rain_falls = json_weathers[:list].map do |weather|
       weather[:rain].present? ? weather[:rain][:'3h'] : nil
     end
-    rainy_weathers.find { |weather| weather[:rain][:'3h'] >= 2 } .present?
+    rain_falls.compact.find { |rain_fall| rain_fall >= RAIN_FALL_JUDGMENT } .present?
+  end
+
+  def umbrella_notice(line_id)
+    message = { type: 'text', text: read_message('umbrella_notice') }
+    client.push_message(line_id, message)
+  end
+
+  def validate_notice_time
+    current_time = Time.zone.now
+    notice_time = params[:notice_time].match(/(\d{2}):(\d{2})/)
+    minute_range = (current_time.min - TOLERANCE_TIME)..(current_time.min + TOLERANCE_TIME)
+    return if notice_time_is_valid?(current_time, notice_time, minute_range)
+
+    render_bad_request
+  end
+
+  def notice_time_is_valid?(current_time, notice_time, minute_range)
+    current_time.hour == notice_time[1].to_i || minute_range.include?(notice_time[2].to_i)
+  end
+
+  def authenticate
+    line_user = LineUser.find_by(line_id: params[:line_id])
+    authenticate_or_request_with_http_token do |token|
+      ActiveSupport::SecurityUtils.secure_compare(token, line_user.token)
+    end
   end
 end
