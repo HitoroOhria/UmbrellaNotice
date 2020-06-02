3437accc4f2930db74dbf659f49a1ab980501038
大平 礼 <hiroto100114@gmail.com>
2020-06-02 10:06:05 +0900

    [debug]production環境でRedisコネクションエラーデバッグのため、sidekiqイニチアライザを環境変数で代替

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,31 @@
 Sidekiq.configure_server do |config|
   case Rails.env
   when 'production'
-    config.redis = {url: 'redis://www.umbrellanotice.work:6379', namespace: 'sidekiq'}
+    case ENV['OPT_HOGE']
+    when 'timeout'
+      config.redis = {url: ENV['HOGEHOGE'], namespace: 'sidekiq', timeout: 5}
+    when 'network_timeout'
+      config.redis = {url: ENV['HOGEHOGE'], namespace: 'sidekiq', network_timeout: 5, timeout: 5}
+    else
+      config.redis = {url: ENV['HOGEHOGE'], namespace: 'sidekiq'}
+    end
   else
-    config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq'}
+    config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq', network_timeout: 5, timeout: 5}
   end
 end
 
 Sidekiq.configure_client do |config|
   case Rails.env
   when 'production'
-    config.redis = {url: 'redis://www.umbrellanotice.work:6379', namespace: 'sidekiq'}
+    case ENV['OPT_HOGE']
+    when 'timeout'
+      config.redis = {url: ENV['HOGEHOGE'], namespace: 'sidekiq', timeout: 5}
+    when 'network_timeout'
+      config.redis = {url: ENV['HOGEHOGE'], namespace: 'sidekiq', network_timeout: 5, timeout: 5}
+    else
+      config.redis = {url: ENV['HOGEHOGE'], namespace: 'sidekiq'}
+    end
   else
-    config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq'}
+    config.redis = {url: 'redis://redis:6379', namespace: 'sidekiq', network_timeout: 5, timeout: 5}
   end
 end
