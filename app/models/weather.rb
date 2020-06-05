947116a7a02fd242d61d985e23bb4527f0fd1857
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-06-05 19:18:07 +0900

    [update]take_forecastで、市名と位置情報のどちらでも取得できるように機能拡張

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -12,22 +12,10 @@ class Weather < ApplicationRecord
   validates :lon, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 },
                   allow_nil: true
 
-  def forecast
-    @forecast ||= take_forecast
-  end
+  attr_accessor :city_forecast
 
-  # OpenWeatherAPI から、天気予報を取得する
-  # 取得できた場合 => 天気予報 Hash
-  # 取得できない場合 => false
-  def take_forecast
-    retry_count = 0
-    begin
-      call_weather_api
-    rescue OpenURI::HTTPError => e
-      retry_count += 1
-      retry_message(e, retry_count)
-      retry_count <= RETRY_CALL_API_COUNT ? (sleep RETRY_CALL_API_WAIT_TIME; retry) : false
-    end
+  def forecast
+    @forecast ||= one_call_api
   end
 
   # 今日の天気が雨である場合 => true
@@ -37,13 +25,14 @@ class Weather < ApplicationRecord
     rain_falls.find { |rain_fall| rain_fall >= RAIN_FALL_JUDGMENT }.present?
   end
 
-  # text が OpenWeatherApi の city に対応しているか検証する
-  # 対応している場合 => 天気予報Hash
+  # text が CurrentWeatherAPI の city に対応しているか検証する
+  # 対応している場合 => true
   # 対応していない場合 => false
-  def validate_city(text)
-    city_name = text.slice(/(.+)[市区]/, 1) || text
-    self.city = to_romaji(city_name)
-    forecast
+  def city_is_valid?(text)
+    city_name     = text.slice(/(.+)[市区]/, 1) || text
+    self.city     = to_romaji(city_name)
+    city_forecast = current_weather_api
+    city_forecast.present?
   end
 
   # OpenWeatherApi の city に対応するローマ字に変換する
@@ -52,30 +41,55 @@ class Weather < ApplicationRecord
     kunrei_moji.gsub(/si/, 'shi').gsub(/ti/, 'chi').gsub(/tu/, 'tsu')
   end
 
-  def save_city
-    save!
-    line_user.update_attribute(:located_at, Time.zone.now)
-  end
-
-  def save_location(lat, lon)
-    self.lat = lat.round(2)
-    self.lon = lon.round(2)
+  # 市名を使用して位置情報設定を行う場合
+  #   => CurrentWeatherAPI を利用して、緯度・経度を保存する
+  # GPS情報を使用して位置情報設定を行う場合
+  #   => GPS情報の意図・経度を保存する
+  def save_location(content)
+    self.lat = (city_forecast.try(:[], :coord).try(:[], :lat) || content[:lat]).round(2)
+    self.lon = (city_forecast.try(:[], :coord).try(:[], :lat) || content[:lon]).round(2)
     save!
     line_user.update_attribute(:located_at, Time.zone.now)
   end
 
   private
 
-  # OpenWeatherAPI の OneCallAPI を呼び出す
-  def call_weather_api
-    base_url = 'http://api.openweathermap.org/data/2.5/onecall?lang=ja'
-    location = "&lat=#{lat}&lon=#{lon}"
-    exclude  = '&exclude=current,minutely,daily'
-    app_id   = "&appid=#{Rails.application.credentials.open_weather_api[:app_key]}"
+  def current_weather_api
+    api_type      = 'weather'
+    request_query = "&q=#{city}"
+    take_api_and_error_handling(api_type, request_query)
+  end
 
-    api_response  = OpenURI.open_uri(base_url + location + exclude + app_id)
+  def one_call_api
+    api_type      = 'onecall'
+    request_query = "&lat=#{lat}&lon=#{lon}&exclude=current,minutely,daily"
+    take_api_and_error_handling(api_type, request_query)
+  end
+
+  # api_type に応じた OpenWeatherAPI から、天気予報を取得する
+  # 天気予報取得時にエラーが発生した場合、3回までリトライする
+  # 取得できた場合 => 天気予報 Hash
+  # 取得できなかった場合 => false
+  def take_api_and_error_handling(api_type, request_query)
+    retry_count = 0
+    begin
+      call_weather_api(api_type, request_query)
+    rescue OpenURI::HTTPError => e
+      retry_count += 1
+      retry_message(e, retry_count)
+      retry_count <= RETRY_CALL_API_COUNT ? (sleep RETRY_CALL_API_WAIT_TIME; retry) : false
+    end
+  end
+
+  # api_type に応じた　OpenWeatherAPI を呼び出す
+  # OneCallAIP の場合、取得したデータに処理を加える
+  def call_weather_api(api_type, request_query)
+    base_url      = "http://api.openweathermap.org/data/2.5/#{api_type}?lang=ja"
+    app_id        = "&appid=#{Rails.application.credentials.open_weather_api[:app_key]}"
+
+    api_response  = OpenURI.open_uri(base_url + request_query + app_id)
     json_forecast = JSON.parse(api_response.read, symbolize_names: true)
-    refill_rain(json_forecast)
+    refill_rain(json_forecast) if api_type == 'onecall'
   end
 
   # OpenWeatherAPI の仕様により、天気が雨以外の場合は雨量が設定されていない
