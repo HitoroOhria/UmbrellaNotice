816592007031a78707e18afdaacb2021b7db81fe
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-06-05 14:21:47 +0900

    [update]天気予報取得時に、雨量が 0 < 3 [mm]の天気は雨から曇りに変更する処理を追加

---
+++ app/controllers/concerns/line_message_helper.rb
@@ -0,0 +1,4 @@
+module LineMessage
+  extend ActiveSupport::Concern
+
+end
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -16,6 +16,27 @@ class Weather < ApplicationRecord
     @forecast ||= take_forecast
   end
 
+  # OpenWeatherAPI から、天気予報を取得する
+  # 取得できた場合 => 天気予報 Hash
+  # 取得できない場合 => false
+  def take_forecast
+    retry_count = 0
+    begin
+      call_weather_api
+    rescue OpenURI::HTTPError => e
+      retry_count += 1
+      retry_message(e, retry_count)
+      retry_count <= RETRY_CALL_API_COUNT ? (sleep RETRY_CALL_API_WAIT_TIME; retry) : false
+    end
+  end
+
+  # 今日の天気が雨である場合 => true
+  # 今日の天気が雨ではない場合 => false
+  def today_is_rainy?
+    rain_falls = forecast[:hourly][0...TAKE_WEATHER_HOUR].map { |hourly| hourly[:rain][:'1h'] }
+    rain_falls.find { |rain_fall| rain_fall >= RAIN_FALL_JUDGMENT }.present?
+  end
+
   # text が OpenWeatherApi の city に対応しているか検証する
   # 対応している場合 => 天気予報Hash
   # 対応していない場合 => false
@@ -27,8 +48,8 @@ class Weather < ApplicationRecord
 
   # OpenWeatherApi の city に対応するローマ字に変換する
   def to_romaji(text)
-    kunrei = Zipang.to_slug(text).gsub(/\-/, '').gsub(/m(?!(a|i|u|e|o|m))/, 'n').to_kunrei
-    kunrei.gsub(/si/, 'shi').gsub(/ti/, 'chi').gsub(/tu/, 'tsu')
+    kunrei_moji = Zipang.to_slug(text).gsub(/\-/, '').gsub(/m(?!(a|i|u|e|o|m))/, 'n').to_kunrei
+    kunrei_moji.gsub(/si/, 'shi').gsub(/ti/, 'chi').gsub(/tu/, 'tsu')
   end
 
   def save_city
@@ -43,39 +64,37 @@ class Weather < ApplicationRecord
     line_user.update_attribute(:located_at, Time.zone.now)
   end
 
-  # 今日の天気が雨である場合 => true
-  # 今日の天気が雨ではない場合 => false
-  def today_is_rainy?
-    rain_falls = forecast[:hourly][0...TAKE_WEATHER_HOUR].map do |hourly|
-      hourly[:rain].present? ? hourly[:rain][:'1h'] : nil
-    end
-    rain_falls.compact.find { |rain_fall| rain_fall >= RAIN_FALL_JUDGMENT }.present?
-  end
-
-  # OpenWeatherAPI から、天気予報を取得する
-  # 取得できた場合 => 天気予報Hash
-  # 取得できない場合 => false
-  def take_forecast
-    retry_count = 0
-    begin
-      call_weather_api
-    rescue OpenURI::HTTPError => e
-      retry_count += 1
-      retry_message(e, retry_count)
-      retry_count <= RETRY_CALL_API_COUNT ? (sleep RETRY_CALL_API_WAIT_TIME; retry) : false
-    end
-  end
-
   private
 
+  # OpenWeatherAPI の OneCallAPI を呼び出す
   def call_weather_api
-    base_url = 'https://api.openweathermap.org/data/2.5/onecall?lang=ja'
-    location = "&lat=#{lat}&lon=#{lon}"
-    exclude  = '&exclude=current,minutely,daily'
-    app_id   = "&appid=#{Rails.application.credentials.open_weather_api[:app_key]}"
+    base_url      = 'https://api.openweathermap.org/data/2.5/onecall?lang=ja'
+    location      = "&lat=#{lat}&lon=#{lon}"
+    exclude       = '&exclude=current,minutely,daily'
+    app_id        = "&appid=#{Rails.application.credentials.open_weather_api[:app_key]}"
 
-    response = OpenURI.open_uri(base_url + location + exclude + app_id)
-    JSON.parse(response.read, symbolize_names: true)
+    api_response  = OpenURI.open_uri(base_url + location + exclude + app_id)
+    json_forecast = JSON.parse(api_response.read, symbolize_names: true)
+    refill_rain(json_forecast)
+  end
+
+  # OpenWeatherAPI の仕様により、天気が雨以外の場合は雨量が設定されていない
+  # 天気が雨であっても、雨量が RAIN_FALL_JUDGMENT [mm] 以下の場合は、曇りとして扱いたい
+  # そのため、以下2つの処理を行う
+  #  - (1) 天気が雨以外の場合、雨量を 0[mm] に設定
+  #  - (2) 雨量が 0 < RAIN_FALL_JUDGMENT [mm] の場合、天気を雨から曇りに変更
+  def refill_rain(json_forecast)
+    json_forecast[:hourly].each do |hourly|
+      hourly[:rain] = { '1h': 0 }  if hourly[:rain].empty?
+      rain_fall     = hourly[:rain][:'1h']
+
+      next if rain_fall.zero? || RAIN_FALL_JUDGMENT <= rain_fall
+
+      hourly[:weather].each do |weather|
+        weather[:main]        = 'Clouds'
+        weather[:description] = 'clouds'
+      end
+    end
   end
 
   def retry_message(exception, retry_count)
@@ -85,7 +104,7 @@ class Weather < ApplicationRecord
       logger.error "この処理は#{RETRY_CALL_API_COUNT}回まで繰り返されます。(現在: #{retry_count}回目)"
     else
       logger.error "[Error]#{exception.class} の再接続に失敗しました。"
-      logger.error exception.backtrase
+      logger.error exception.backtrace
     end
   end
 end
