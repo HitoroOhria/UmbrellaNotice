3b76db1ade000132718dca6b7ee876e69d8aa885
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-06-06 16:59:49 +0900

    [hotfix]Weatherモデルのlatカラムの範囲が -45 .. 45 になっていたミスを修正
      - -90 .. 90 に変更

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -7,7 +7,7 @@ class Weather < ApplicationRecord
   belongs_to :user,      optional: true
   belongs_to :line_user, optional: true
 
-  validates :lat, numericality: { greater_than_or_equal_to: -45, less_than_or_equal_to: 45 },
+  validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 },
                   allow_nil: true
   validates :lon, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 },
                   allow_nil: true
