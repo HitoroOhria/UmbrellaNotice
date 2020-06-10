0bcfc6e161e90806628e1f4cbef45c036d124e82
Hitoro Ohira <hiroto100114@gmail.com>
2020-06-10 20:15:12 +0900

    [update]本番環境では、S3の画像を使用するように更新
      - StaticPagesHelper#image_url(file_name)
      - cledentials -> bucket, access, secret :add

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,15 @@
 module StaticPagesHelper
+  def image_url(file_name)
+    case Rails.env
+    when 'production'
+      s3_secret   = Rails.application.credentials.aws.s3
+      credentials = Aws::Credentials.new(s3_secret[:access_key], s3_secret[:secret_access_key])
+      client      = Aws::S3::Client.new(credentials: credentials)
+      signer      = Aws::S3::Presigner.new(client: client)
+      obj_key     = "images/#{file_name}.png"
+      signer.presigned_url(:get_object, bucket: s3_secret[:bucket], key: obj_key, expires_in: 60)
+    when 'development'
+      file_name
+    end
+  end
 end
