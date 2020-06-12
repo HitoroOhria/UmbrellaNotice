5d6607bf3263d48a6c9007bfac4d9518541dd2ed
Hitoro Ohira <hiroto100114@gmail.com>
2020-06-12 23:27:50 +0900

    [add]User#showにインスタンス変数を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  # /usres/:id
+  # /users/:id
   def show
-    @user    = current_user
-    @user_id = session['user_id']
+    @user      = current_user
+    @calendar  = @user.calendar
+    @weather   = @user.weather
+    @line_user = @weather.line_user
+    @user_id   = session['user_id']
   end
 end
