840ae1d05ea7c4403df005b3351cfea963e145f6
Hitoro Ohira <hiroto100114@gmail.com>
2020-06-16 20:11:21 +0900

    [fix]RichMenuableのメソッドを、引数を受け取る用に修正

--- app/controllers/concerns/rich_menuable.rb
+++ app/controllers/concerns/rich_menuable.rb
@@ -1,36 +1,37 @@
 module RichMenuable
   extend ActiveSupport::Concern
 
-  def rich_menus
-    send(event['postback']['data'])
+  def rich_menus(event, line_user)
+    send(event['postback']['data'], event, line_user)
   end
 
-  def reply_weather_forecast
+  def reply_weather_forecast(_event, line_user)
     reply('notice_weather', line_user: line_user, weather: line_user.weather)
   end
 
-  def notice_time_setting
+  def notice_time_setting(event, line_user)
     target_time = event['postback']['params']['time']
+
     line_user.update_attribute(:notice_time, target_time)
-    reply('completed_notice_time_setting')
+    reply('completed_notice_time_setting', line_user: line_user.reload)
   end
 
-  def toggle_silent_notice
+  def toggle_silent_notice(_event, line_user)
     boolean = line_user.silent_notice
     line_user.update_attribute(:silent_notice, !boolean)
-    reply('completed_toggle_silent_notice')
+    reply('completed_toggle_silent_notice', 'description_silent_notice', line_user: line_user)
   end
 
-  def location_resetting
-    line_user.locating_at = Time.zone.now
+  def location_resetting(_event, line_user)
+    line_user.update_attribute(:locating_at, Time.zone.now)
     reply('location_resetting')
   end
 
-  def issue_serial_number
+  def issue_serial_number(_event, line_user)
     reply('issue_serial_number', line_user: line_user)
   end
 
-  def profile_page
-    redirect_to user_url(line_user.user)
+  def profile_page(_event, line_user)
+    (user = line_user.user) ? redirect_to(user) : reply('unknown_user')
   end
 end
