359c76f89f983715bee4fb6fe7c18794be8c2074
Hitoro Ohira <hiroto100114@gmail.com>
2020-06-20 14:05:52 +0900

    [add]ユーザー登録時にCalendarモデルを作成するロジックを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,6 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
+      Calendar.create(user: @user)
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
