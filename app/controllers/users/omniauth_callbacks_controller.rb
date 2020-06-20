dd61014d806e14989154ef676f5e3b7fa59bbbcc
Hitoro Ohira <hiroto100114@gmail.com>
2020-06-20 19:04:59 +0900

    [clean]Users::OmniauthCallbacksController#facebookをリファクタリング

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,14 +3,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    else
-      Calendar.create(user: @user)
-      session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
-    end
+    @user.persisted? ? exit_user(@user) : new_user(@user)
+  end
+
+  def exit_user(user)
+    sign_in_and_redirect user, event: :authentication
+    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+  end
+
+  def new_user(user)
+    Calendar.create(user: user)
+    session['devise.facebook_data'] = request.env['omniauth.auth']
+    redirect_to new_user_registration_url
   end
 
   def failure
