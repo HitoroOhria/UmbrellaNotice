c4ae336dde5a1f959d3202a866e4a8d41e38d4e8
Hiroto Ohira <hiroto100114@gmail.com>
2020-07-11 15:17:10 +0900

    [update]LineUserが残っているため、User削除にWeatherを削除しないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :omniauthable
 
-  has_one :weather,  dependent: :destroy
+  has_one :weather
   has_one :calendar, dependent: :destroy
 
   delegate :line_user, to: :weather, allow_nil: true
