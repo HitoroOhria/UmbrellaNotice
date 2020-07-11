463ac1e3f5293a81123a3c98119f9c613f028fa6
Hiroto Ohira <hiroto100114@gmail.com>
2020-07-11 20:22:36 +0900

    [clean]facebokログインができないバグを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,41 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :omniauthable
+         :confirmable, :omniauthable, omniauth_providers: %i[facebook]
 
   has_one :weather
   has_one :calendar, dependent: :destroy
 
   delegate :line_user, to: :weather, allow_nil: true
 
+  def self.from_line_login(email, line_id)
+    user = find_by(email: email)
+    return user if user
+
+    user = create_from_api_login(email)
+    Calendar.create(user: user) && user.relate_line_user(user, line_id) && user
+  end
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid      = auth.uid
-      user.email    = auth.info.email
+    user = find_by(email: auth.info.email)
+    return user if user
+
+    user = create_from_api_login(auth.info.email)
+    Calendar.create(user: user) && user
+  end
+
+  def self.create_from_api_login(email)
+    create do |user|
+      user.email    = email
       user.password = Devise.friendly_token[0, 20]
-      user.name     = auth.info.name
-      user.image    = auth.info.image
       user.skip_confirmation!
     end
+  end
+
+  private
+
+  def relate_line_user(user, line_id)
+    line_user = LineUser.find_by(line_id: line_id)
+    line_user.weather.update_attribute(user_id: user.id)
   end
 end
