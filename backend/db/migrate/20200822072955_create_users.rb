8045c7cad49ed0b1f9f5d0c60541894414ca31f5
Hiroto Ohira <hiroto100114@gmail.com>
2020-08-22 17:01:58 +0900

    [add]rails g model user email

---
+++ backend/db/migrate/20200822072955_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+
+      t.timestamps
+    end
+  end
+
+  add_index :users, :email, unique: true
+end
