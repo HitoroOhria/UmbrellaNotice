591f57aedd692c2f33848d1dd91351044d6e6e78
Hiroto Ohira <hiroto100114@gmail.com>
2020-08-23 16:39:07 +0900

    [clean]cacheストアの設定を修正

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -3,17 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  redis_setting = proc { |namespace, expire_time|
-    {
-      servers: {
-        host: ENV['REDIS_CACHE_HOST'],
-        port: ENV['REDIS_CACHE_PORT'],
-        namespace: namespace
-      },
-      expire_in: expire_time
-    }
-  }
-  config.cache_store = :redis_store, redis_setting.call('cache', 3.month)
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -59,6 +48,17 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  redis_setting = proc { |namespace, expire_time|
+    {
+      servers: {
+        host: ENV['REDIS_CACHE_HOST'],
+        port: ENV['REDIS_CACHE_PORT'],
+        namespace: namespace
+      },
+      expire_in: expire_time
+    }
+  }
+  config.cache_store = :redis_cache_store, redis_setting.call('cache', 3.month)
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
