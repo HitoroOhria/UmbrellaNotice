ff8a593b686e8f03913d638e2d7757c157e3325a
Hiroto Ohira <hiroto100114@gmail.com>
2020-09-08 15:30:26 +0900

    [update]不要にデータを作成してしまっていたため、seeds.rbを更新

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,6 +1,6 @@
 user      = User.find_or_create_by!(email: 'test@example.com')
-line_user = LineUser.find_or_create_by!(user: user, line_id: Faker::Food.fruits)
-Weather.find_or_create_by!(line_user: line_user, city: '渋谷区', lat: 39.96 , lon: 140.85)
+line_user = user.line_user || LineUser.create!(user: user, line_id: Faker::Food.fruits)
+line_user.weather || Weather.create!(line_user: line_user, city: '渋谷区', lat: 39.96 , lon: 140.85)
 # Calendar.create!(user: user)
 
 # LINE API の手動テストのために、ENV['LINE_ID'] があれば LineUser を作成する
